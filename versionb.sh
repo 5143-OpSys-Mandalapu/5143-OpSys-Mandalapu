43a23de254ef6eb6aee3c466a579e0d490a5cded
Vishnu Chaitanya Mandalapu <vishnuchaitanyam@gmail.com>
2016-02-10 01:24:27 -0600

    Create versionb.sh

---
+++ versionb.sh
@@ -0,0 +1,9 @@
+file_name=$@
+#newFile="$file_name_"
+justname="${file_name##*/}"
+
+#echo "${justname%.*}"
+fileExt="${file_name##*.}"
+
+newFile="${justname%.*}_$(date +%y-%m-%d).$fileExt"
+cp $file_name $newFile
