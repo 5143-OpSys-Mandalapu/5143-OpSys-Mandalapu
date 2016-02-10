0934fb1319a50559a4a63c7926f35a09fc648e42
Vishnu Chaitanya Mandalapu <vishnuchaitanyam@gmail.com>
2016-02-10 01:22:46 -0600

    Create command_args.sh

---
+++ command_args.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo command_args.sh #print the name of the file
+echo \(Contents of arg1\) #print the line to the prompt
+for i;  # creating a loop for all the args to print in the next line
+do
+echo $i #printing the args from the command line
+done  # end of for loop
